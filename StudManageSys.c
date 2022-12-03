b31a548a48b64cf234f91e8855a15433e4452822
suhel khan <yinyingdark@gmail.com>
2022-12-04 02:33:43 +0530

    Updated code
    Added code of student detail
    and more...

--- StudManageSys.c
+++ StudManageSys.c
@@ -4,25 +4,80 @@
 
 int main()
 {
+    // ======Declaring Variable for username and password (GLOBAL VARIABLE)=======
     char userName[50], pwd[50];
 
+    // =======(\xdb) is used to display better UI=======
     printf("\n \xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb  LOGIN FIRST \xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\n ");
 
     printf("Username:");
     scanf("%s", &userName);
+
     printf("\nPassword:");
     scanf("%s", &pwd);
 
+    // =====(strcmp) is used to compare string =====
+
     if (strcmp(userName, "KCCITM") == 0 && strcmp(pwd, "admin@123") == 0)
     {
+        // ===== Declaring variable (for HOME page *NOT GLOBAL*) =====
         int opt;
+
         // clrscr();
+
         printf("\n1.Add Student Details\n 2.Preview student details \n 3.Grades of the students\n 4.Total Number of students\n");
+
         scanf("%d", &opt);
+
+        // Mani Menu in switch case
+
         switch (opt)
         {
-        case 1:
-            printf("option 1");
+
+            // ========= Declaring var fot students details =========
+            char sName[100], regNo[100], doB[100], fName[100], mName[100], add[100], crsEnrol[100], dofEnrol[100];
+
+        case 1: // ===== To Add student detail =====
+
+            printf("\t\t---------------|Add Student Details|---------------\t\t\n");
+
+            printf("Name of the student:");
+            scanf("%s", &sName);
+
+            printf("\nRegistration number:");
+            scanf("%s", &regNo);
+
+            printf("\nDate of Birth:");
+            scanf("%s", &doB);
+
+            printf("\nFather's name:");
+            scanf("%s", &fName);
+
+            printf("\nMother's name:");
+            scanf("%s", &mName);
+
+            printf("\nAddress:");
+            scanf("%s", &add);
+
+            printf("\nCourse enrolled:");
+            scanf("%s", &crsEnrol);
+
+            printf("\nDate of enrollment:");
+            scanf("%s", &dofEnrol);
+
+            // ===== Preview students details =====
+
+            printf("\n\n\xdb Details preview \xdb\n");
+            
+            printf("\nName of the student:%s\n", sName);
+            printf("\nRegistration number:%s\n", regNo);
+            printf("\nDate of birth:%s\n", doB);
+            printf("\nFather's name:%s\n", fName);
+            printf("\nMother's name:%s\n", mName);
+            printf("\nAddress:%s\n", add);
+            printf("\nCourse enrolled:%s\n", crsEnrol);
+            printf("\nDate of enrollment:%s\n", dofEnrol);
+
             break;
 
         case 2:
